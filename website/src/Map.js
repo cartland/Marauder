a1e1e83adadfc5c1528254decf424ee1e78fe17f
Andrew Stromme <andrew.stromme@gmail.com>
2019-09-08 22:01:48 -0700

    show people on the map, very basic

--- website/src/Map.js
+++ website/src/Map.js
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
 
+let rooms = {
+  kitchen: {
+    topLeft: {
+      x: 486,
+      y: 439,
+    },
+    height: 563,
+    width: 386,
+    name: 'Kitchen',
+  },
+  living_room: {
+    topLeft: {
+      x: 872,
+      y: 439,
+    },
+    height: 563,
+    width: 420,
+    name: 'Living Room',
+  },
+}
+
 class Canvas extends React.Component {
+  state = {
+    people: {
+      stromme: 'living_room',
+      cartland: 'kitchen',
+    },
+  }
 
   constructor(props) {
     super(props);
@@ -10,11 +37,29 @@ class Canvas extends React.Component {
   }
 
   handleOnLoadImage = (e) => {
+    this.draw()
+  }
+
+  draw = () => {
     console.log(this.canvas, this.image)
     let ctx = this.canvas.current.getContext("2d")
-    ctx.drawImage(e.target, 0, 0);
+    ctx.drawImage(this.image.current, 0, 0);
     ctx.font = "40px Courier";
-    ctx.fillText("Hello Map", 210, 75);
+    ctx.fillText("Make Your Mischief", 210, 75);
+
+
+    Object.keys(this.state.people).map(person => {
+      let personRoom = this.state.people[person];
+      let roomDetails = rooms[personRoom];
+
+      let personX = roomDetails.topLeft.x + roomDetails.width / 4;
+      let personY = roomDetails.topLeft.y + roomDetails.height / 4;
+
+      ctx.beginPath();
+      ctx.arc(personX, personY, 10, 0, 2*Math.PI, true);
+      ctx.fill();
+      ctx.fillText(person, personX+10, personY+35);
+    })
   }
 
   render() {
