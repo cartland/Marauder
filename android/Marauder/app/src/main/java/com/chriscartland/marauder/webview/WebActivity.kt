614378a37c46ca40c14fbafb418001fae5ae13ab
cartland <c.cartland@gmail.com>
2019-10-26 16:39:01 -0700

    Android app will reset when a wand logical ID "reset" is sent

--- android/Marauder/app/src/main/java/com/chriscartland/marauder/webview/WebActivity.kt
+++ android/Marauder/app/src/main/java/com/chriscartland/marauder/webview/WebActivity.kt
@@ -10,6 +10,9 @@ import android.webkit.WebView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.chriscartland.marauder.R
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Date
 
 
 class WebActivity : AppCompatActivity() {
@@ -17,6 +20,7 @@ class WebActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var dimensionText: TextView
     private val HIDE_SYSTEM_UI_DELAY_MS: Long = 3000
+    private val RESET_LOGICAL_ID: String = "reset"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
@@ -26,6 +30,56 @@ class WebActivity : AppCompatActivity() {
         dimensionText = findViewById(R.id.dimension_text)
         logWindowDimensions()
         configureWebView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+        watchForResetTrigger()
+    }
+
+    private var lastResetTimestamp: Timestamp? = null
+
+    private fun watchForResetTrigger() {
+        Log.d(TAG, "watchForResetTrigger")
+        val nfcUpdates = FirebaseFirestore.getInstance()
+            .collection("nfcUpdates")
+            .whereGreaterThan("timestamp", Date())
+        nfcUpdates.addSnapshotListener { snapshot, e ->
+            Log.d(TAG, "watchForResetTrigger: nfcUpdates.addSnapshotListener")
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && !snapshot.isEmpty) {
+                Log.d(TAG, "watchForResetTrigger: Snapshot exists, looking at documents")
+                snapshot.documents.forEach { document ->
+                    Log.d(TAG, "watchForResetTrigger: Found a document")
+                    val timestamp = document["timestamp"] as Timestamp?
+                    if (timestamp == null) {
+                        Log.d(TAG, "watchForResetTrigger: Igorning null timestamp")
+                        return@forEach
+                    }
+                    val lastReset = lastResetTimestamp
+                    if (lastReset != null && timestamp <= lastReset) {
+                        Log.d(TAG, "watchForResetTrigger: Ignoring old reset, last $lastReset this $timestamp")
+                        return@forEach
+                    }
+                    lastResetTimestamp = timestamp
+                    val nfcData = document["nfcData"] as? Map<String, String>
+                    val logicalId = nfcData?.get("nfcLogicalId")
+                    if (logicalId == RESET_LOGICAL_ID) {
+                        Log.d(TAG, "watchForResetTrigger: Logical ID is a reset request")
+                        configureWebView()
+                    } else {
+                        Log.d(TAG, "watchForResetTrigger: Logical ID is not a reset request: $logicalId")
+                    }
+                }
+            } else {
+                Log.d(TAG, "watchForResetTrigger: Snapshot does not exist")
+            }
+        }
     }
 
     private fun logWindowDimensions() {
